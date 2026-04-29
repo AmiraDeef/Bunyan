6975e2a279b88bbf5c05d1a1818a898b25caa2a4
AmiraDeef <amiradeef4@gmail.com>
2026-04-29 14:01:23 +0300

    init Bunyan website and Dashboard

---
+++ Dashboard/src/components/layout/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import Logo from "../../../assets/axis-logo.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faX, faBell } from '@fortawesome/free-solid-svg-icons'
+import React from 'react'
+import Style from './Navbar.module.css'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav>
+                <div className="d-flex justify-content-between align-items-center ">
+                    <div className={Style.NavLogo} className='d-flex px-5'>
+                        <img src={Logo} alt="logo" />
+                    </div>
+                    <div className='d-flex px-5 align-items-center' >
+                        <FontAwesomeIcon icon={faBell} />
+
+                        <div class="dropdown">
+                            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Supper Admin
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a className="dropdown-item active" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#">Separated link</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+
+            </nav>
+
+        </>
+    )
+}
---
+++ client-side/src/App.jsx
@@ -0,0 +1,18 @@
+import HeroSection from './components/pages/HeroSection'
+import  Navbar  from './components/Layouts/Navbar'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+    <HeroSection/>
+
+      
+    </>
+  )
+}
+
+export default App
